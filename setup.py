9158be96efa4c058f8936863cd005675e3b4d0de
Alice Isabel <roridev@protonmail.com>
2024-01-29 20:41:19 -0300

    Arruma nubisse

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="discord_oauth_weblate",
     version="0.0.1",
-    packages=["discord_oauth_weblate"],
+    packages=["discord_oauth_weblate", "discord_oauth_weblate.backends"],
     include_package_data=True,
     license="Unlicense",
     description="Discord Oauth support for Weblate",
